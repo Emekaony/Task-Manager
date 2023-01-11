0e3a54e609d441016ce8d87f94ae46037c952315
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-01-11 02:08:08 -0800

    Working on REST API

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,27 +1,63 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 mongoose.connect("mongodb://127.0.0.1:27018/task-manager-api", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+// });
+
+// const me = new User({
+//   name: "Nnaemeka",
+//   age: 21,
+// });
+
+// me.save()
+//   .then((res) => {
+//     console.log("me", res);
+//   })
+//   .catch((err) => {
+//     console.log("error", err);
+//   });
+const Task = mongoose.model("Tasks", {
+  description: {
     type: String,
+    required: true,
   },
-  age: {
-    type: Number,
+  completed: {
+    type: Boolean,
+  },
+  purpose: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
   },
 });
 
-const me = new User({
-  name: "Nnaemeka",
-  age: 21,
+const newTask = new Task({
+  description: "This will work",
+  purpose: "Nnaemekaonyeokoro@outlook.com",
 });
 
-me.save()
+newTask
+  .save()
   .then((res) => {
-    console.log("me", res);
+    console.log("New task has been saved", res);
   })
   .catch((err) => {
-    console.log("error", err);
+    console.log(
+      "There was an error saving the instance of the Task model",
+      err
+    );
   });
