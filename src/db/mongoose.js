551896d3d23890bca84a190c5c6388f51b58ba1a
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-01-12 11:11:19 -0800

    Set up basic express server

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+
 mongoose.connect("mongodb://127.0.0.1:27018/task-manager-api", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -8,15 +9,47 @@ mongoose.connect("mongodb://127.0.0.1:27018/task-manager-api", {
 // const User = mongoose.model("User", {
 //   name: {
 //     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Must enter a valid email");
+//       }
+//     },
 //   },
 //   age: {
 //     type: Number,
+//     default: 0,
+//     required: true,
+//     // we get as the first argument the value we are validating
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error("age must be greater than 0");
+//       }
+//     },
+//   },
+//   password: {
+//     type: String,
+//     trim: true,
+//     required: true,
+//     validate(value) {
+//       if (!value > 6 || !value.includes("password")) {
+//         throw new Error("Invalid password field");
+//       }
+//     },
 //   },
 // });
 
 // const me = new User({
-//   name: "Nnaemeka",
-//   age: 21,
+//   name: "   Emeka   ",
+//   password: "12345",
+//   email: "MIKE@outLook.compassword  ",
 // });
 
 // me.save()
@@ -26,38 +59,28 @@ mongoose.connect("mongodb://127.0.0.1:27018/task-manager-api", {
 //   .catch((err) => {
 //     console.log("error", err);
 //   });
-const Task = mongoose.model("Tasks", {
+
+const Task = mongoose.model("Task", {
   description: {
     type: String,
     required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
-  },
-  purpose: {
-    type: String,
-    required: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
+    default: false,
   },
 });
 
-const newTask = new Task({
-  description: "This will work",
-  purpose: "Nnaemekaonyeokoro@outlook.com",
+const task1 = new Task({
+  description: "Be fucking consistent with coding",
 });
 
-newTask
+task1
   .save()
   .then((res) => {
-    console.log("New task has been saved", res);
+    console.log("Saved successifully", res);
   })
-  .catch((err) => {
-    console.log(
-      "There was an error saving the instance of the Task model",
-      err
-    );
+  .catch((e) => {
+    console.log("There was an error saving the document");
   });
