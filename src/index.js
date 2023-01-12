1789ccba62a1b707943f73f759889cc415238fad
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-01-12 11:32:03 -0800

    Used express middleware to parse incoming data from post request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// allows express parse incoming data
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  res.send("Welcome to the app");
+});
+
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
