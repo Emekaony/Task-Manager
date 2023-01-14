1c14fa47683909abe113021433afa4a4309b40ca
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-01-14 12:43:30 -0800

    Sepeared users and tasks into separate route files

---
+++ src/routers/tasks.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const Task = require("../models/tasks");
+const router = new express.Router();
+
+// MAKE SURE TO SWITCH FROM `app` TO `router`
+
+// resource endpoint for task creation
+router.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/tasks/", async (req, res) => {
+  try {
+    const result = await Task.find({});
+    if (!result) {
+      return res.status(404).send();
+    }
+    res.status(200).send(result);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send("No user with that id exists");
+    }
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(404).send(e);
+  }
+});
+
+// use patch to update a resource
+router.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidUpdate = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  // if the update is now allowed, send a 404 and stop there
+  if (!isValidUpdate) {
+    return res.status(400).send("Attempting to perform an invalid update");
+  }
+
+  try {
+    // setting new: true will return the new user with the updates routerlied
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send("User does not exist");
+    }
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(500).send("Error deleting user");
+  }
+});
+
+module.exports = router;
---
+++ src/routers/user.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Home, but from the users router");
+});
+
+// resource endpoint for user creation
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// reads the whole users collection and sends it to the client
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send();
+    }
+    res.status(201).send(users);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// gets a user with a specific id - Note the difference between a route param and a request param
+// this here is a request parameter
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const foundUser = await User.findById(_id);
+    if (!foundUser) {
+      return res.status(404).send();
+    }
+    res.send(foundUser);
+  } catch (e) {
+    res.status(500).send(`Could not find user with id: ${_id}`);
+  }
+});
+
+// use patch to update a resource
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "age", "email", "password"];
+  const isValidUpdate = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  // if the update is now allowed, send a 404 and stop there
+  if (!isValidUpdate) {
+    return res.status(400).send("Attempting to perform an invalid update");
+  }
+
+  try {
+    // setting new: true will return the new user with the updates routerlied
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// delete a user
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send("User does not exist");
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send("Error deleting user");
+  }
+});
+
+module.exports = router;
